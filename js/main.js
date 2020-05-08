73e40ce9b407fb6d852be33609bd0f5b4114a10c
Roman00 <rvasin00@gmail.com>
2020-05-08 17:28:02 +0600

    feat: add a search

--- js/main.js
+++ js/main.js
@@ -22,6 +22,8 @@ const cardsRestaurants = document.querySelector('.cards-restaurants'),
   minPrice = document.querySelector('.price'),
   category = document.querySelector('.category');
 
+const inputSearch = document.querySelector('.input-search');
+
 
 let login = localStorage.getItem('gloDelivery');
 
@@ -210,6 +212,58 @@ function init() {
     containerPromo.classList.remove('hide');
     restaurants.classList.remove('hide');
     menu.classList.add('hide');
+  });
+
+  inputSearch.addEventListener('keydown', function(event) {
+    if(event.keyCode === 13) {
+      const target = event.target;
+      const value = target.value.toLowerCase().trim();
+
+      target.value = '';
+      if(!value || value.length < 3) {
+        target.style.backgroundColor = 'tomato';
+        setTimeout(function(){
+          target.style.backgroundColor = '';
+        }, 2000);
+        return;
+      }
+
+      const goods = [];
+
+      getData('./db/partners.json')
+      .then((data) => {
+        const products = data.map(function(item){
+          return item.products;
+        });
+
+        products.forEach(function(product){
+          getData(`./db/${product}`)
+          .then(function(data){
+            goods.push(...data);
+
+            const searchGoods = goods.filter(function(item){
+              return item.name.toLowerCase().includes(value);
+            });
+
+            cardsMenu.textContent = '';
+
+            containerPromo.classList.add('hide');
+            restaurants.classList.add('hide');
+            menu.classList.remove('hide');
+
+            restaurantTitle.textContent = 'Результаты поиска';
+            rating.textContent = '';
+            minPrice.textContent = '';
+            category.textContent = '';
+
+            return searchGoods;
+          })
+          .then(function(data){
+            data.forEach(createCardGood);
+          });
+        });
+      });
+    }
   });
 
   const initSwiper = () => {
